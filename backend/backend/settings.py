044c9f19f3d48fbdb2eb05e25a6e0e2fec3f362c
RNSiHub <sanjay.n.ihub@snsgroups.com>
2025-06-29 23:18:58 +0530

    frontend vite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,8 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5173"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
